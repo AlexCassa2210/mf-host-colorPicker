a4a1f23d24d6a89971258a380bfba0cd58c70ce7
Alex Cascante <alexcascante.689@yahoo.com>
2023-12-30 21:02:35 -0600

    publish host mf

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import ColorPicker from 'colorPicker/ColorPicker';
+import ColorList from 'colorList/ColorList';
+
+import {useColors} from 'colorPicker/useColors';
+
+const App = () => {
+
+  const { color, colorsList, handleChangeColor, handleSubmitSaveColor} = useColors()
+
+  return (
+    <>
+      <h1 className="text-center bg-dark text-white p-2">Color Picker</h1>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-12 col-md-4">
+            <ColorList colorsList={colorsList} />
+          </div>
+          <div className="col-12 col-md-8">
+            <ColorPicker
+              color={color}
+              handleChangeColor={handleChangeColor}
+              handleSubmitSaveColor={handleSubmitSaveColor}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
